bfdb60119036aaac776704447b4fa6b8c11b21b9
autumn-trip <harun107@naver.com>
2025-01-16 11:59:40 +0900

    Add files via upload
    update

---
+++ moneybricks/src/main/java/com/moneybricks/MoneybricksApplication.java
@@ -0,0 +1,13 @@
+package com.moneybricks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneybricksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneybricksApplication.class, args);
+    }
+
+}
