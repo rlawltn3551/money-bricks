31afe7b976c79ad7624117c45577f1ace01ec769
autumn-trip <harun107@naver.com>
2025-01-16 12:01:50 +0900

    Add files via upload
    update front

---
+++ moneybricks_front/src/test/java/com/moneybricks/MoneybricksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moneybricks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneybricksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
